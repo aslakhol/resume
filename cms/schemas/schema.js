42622e697381e3c476bd1cd10bb0360ee8e178b6
Aslak Hollund <aslak.hollund@nav.no>
2020-07-06 09:21:02 +0200

    include new schemas

--- cms/schemas/schema.js
+++ cms/schemas/schema.js
@@ -5,6 +5,10 @@ import createSchema from "part:@sanity/base/schema-creator";
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
 import animal from "./animal";
+import organization from "./organization";
+import project from "./project";
+import responsibility from "./responsibility";
+import contact from "./contact";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -15,5 +19,9 @@ export default createSchema({
   types: schemaTypes.concat([
     /* Your types here! */
     animal,
+    contact,
+    organization,
+    project,
+    responsibility,
   ]),
 });
