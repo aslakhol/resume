ce2f5a646fb2412004200f85fbb0b86a271e7ab9
Aslak Hollund <aslak.hollund@nav.no>
2020-07-06 09:21:48 +0200

    add program schema

---
+++ cms/schemas/program.js
@@ -0,0 +1,35 @@
+export default {
+  name: "program",
+  title: "Program",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+      description: "Name of program.",
+    },
+    {
+      name: "institution",
+      title: "Institution",
+      type: "reference",
+      of: [{ type: "organization" }],
+      description: "The institution the program belongs to.",
+    },
+    {
+      name: "start_date",
+      title: "Start date",
+      type: "date",
+      description: "The date I started on the program.",
+    },
+    {
+      name: "end_date",
+      title: "End date",
+      type: "date",
+      description: "The date I ended the program, leave blank if ongoing.",
+    },
+  ],
+  preview: {
+    select: { title: "name" },
+  },
+};
