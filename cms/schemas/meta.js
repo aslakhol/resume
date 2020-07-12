1d565e9ca38c4a345c0c5362c765e224c728c0c0
Aslak Hollund <aslak.hollund@nav.no>
2020-07-12 21:42:35 +0200

    add meta fields

---
+++ cms/schemas/meta.js
@@ -0,0 +1,64 @@
+export default {
+  name: "meta",
+  title: "Meta",
+  type: "document",
+  fields: [
+    {
+      name: "first_name",
+      title: "First name",
+      type: "string",
+      description: "My first name.",
+    },
+    {
+      name: "last_name",
+      title: "Last name",
+      type: "string",
+      description: "My last name.",
+    },
+    {
+      name: "email",
+      title: "Email",
+      type: "email",
+      description: "My email.",
+    },
+    {
+      name: "linkedin",
+      title: "LinkedIn",
+      type: "url",
+      description: "LinkedIn url.",
+    },
+    {
+      name: "website",
+      title: "Website",
+      type: "url",
+      description: "Personal website.",
+    },
+    {
+      name: "github",
+      title: "Github",
+      type: "url",
+      description: "Github url.",
+    },
+    {
+      name: "phone",
+      title: "Phone number",
+      type: "string",
+      description: "My phone number.",
+    },
+    {
+      name: "dob",
+      title: "Date of birth",
+      type: "date",
+      description: "My date of birth.",
+    },
+    {
+      name: "address",
+      title: "Address",
+      type: "string",
+      description: "My address.",
+    },
+  ],
+  preview: {
+    select: { title: "first_name" },
+  },
+};
