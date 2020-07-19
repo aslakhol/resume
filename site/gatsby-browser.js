d30d26f58f18aa68a8b1bd70ea1635cd30e3f16c
Aslak Hollund <aslak.hollund@nav.no>
2020-07-19 23:46:13 +0200

    add new fonts

--- site/gatsby-browser.js
+++ site/gatsby-browser.js
@@ -4,4 +4,6 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+require("typeface-roboto")
+require("typeface-source-sans-pro")
+require("typeface-source-code-pro")
