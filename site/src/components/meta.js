602c15ab333ae5e309fc8c3e38719bfdb6291fe9
Aslak Hollund <aslak.hollund@nav.no>
2020-07-23 21:33:26 +0200

    create contact elements

--- site/src/components/meta.js
+++ site/src/components/meta.js
@@ -1,5 +1,10 @@
 import React from "react"
 import { graphql } from "gatsby"
+import GitHubIcon from "../images/svg/github.svg"
+import LinkedInIcon from "../images/svg/linkedin.svg"
+import MailIcon from "../images/svg/mail.svg"
+import PhoneIcon from "../images/svg/phone.svg"
+
 import "./meta.css"
 
 const Meta = props => {
@@ -22,21 +27,48 @@ const Meta = props => {
       </h1>
       <span className="address">{address}</span>
       <span className="contact">
-        <span>{phone}</span> | <span>{email}</span> |{" "}
-        <span>
-          <a href={github.url}>{github.username}</a>
-        </span>{" "}
-        |{" "}
-        <span>
-          <a href={linkedin.url}>{linkedin.username}</a>
-        </span>{" "}
-        | <span>{website}</span>
+        <ContactElement displayString={phone}>
+          <PhoneIcon />
+        </ContactElement>
+        <span>|</span>
+        <ContactElement displayString={email}>
+          <MailIcon />
+        </ContactElement>
+        <span>|</span>
+        <ContactElementLink link={github}>
+          <GitHubIcon />
+        </ContactElementLink>
+        <span>|</span>
+        <ContactElementLink link={linkedin}>
+          <LinkedInIcon />
+        </ContactElementLink>
       </span>
     </div>
   )
 }
 
 export default Meta
+
+const ContactElementLink = props => {
+  const { link, children } = props
+
+  return (
+    <span className="contact-element">
+      {children}
+      <a href={link.url}>{link.username}</a>
+    </span>
+  )
+}
+const ContactElement = props => {
+  const { displayString, children } = props
+
+  return (
+    <span className="contact-element">
+      {children}
+      <span>{displayString}</span>
+    </span>
+  )
+}
 
 export const query = graphql`
   fragment MetaFragment on SanityMeta {
