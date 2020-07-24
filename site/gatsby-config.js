04d3eb17894571ad8c49e4c6748d3e424dccaa12
Aslak Hollund <aslak.hollund@nav.no>
2020-07-24 17:26:20 +0200

    update manifest, need to find new favicon

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -18,11 +18,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `aslak-hollund-portfolio`,
+        short_name: `porfolio`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#333333`,
+        theme_color: `#333333`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
